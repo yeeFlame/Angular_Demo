1d53938ceff1a5003882a2728b93377c5e47d3f7
刘纪平 <jipings@outlook.com>
2018-11-14 16:44:53 +0800

    add click

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,13 +9,17 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor() { 
+  }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
   heroes = HEROES;
-
-  constructor() { 
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   ngOnInit() {
