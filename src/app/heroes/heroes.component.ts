908e29157ef59a253a65ce7ce3b4eef3c790629e
刘纪平 <jipings@outlook.com>
2018-11-15 11:33:11 +0800

    添加api请求方式

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,21 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor() { 
+  constructor(private heroService: HeroService) { 
   }
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   
   onSelect(hero: Hero): void {
@@ -24,6 +24,11 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     console.log('init hero component')
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeros()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
